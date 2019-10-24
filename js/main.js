bd2f78985e6af23d6c976ff89ddc047abb2d31e8
Pradip Poudyal <53552973+Priuel@users.noreply.github.com>
2019-10-24 18:05:43 +1100

    added java script

---
+++ js/main.js
@@ -0,0 +1,64 @@
+// for slider starts
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 1,
+                nav: true,
+                loop: false
+            }
+        }
+    })
+});
+
+// for slider ends
+
+
+// smooth scroll starts
+
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            // event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+
+
+});
+
+
+$(".smooth-btn").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 900);
+});
